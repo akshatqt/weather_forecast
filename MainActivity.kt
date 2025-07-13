73fc080a5c650370129e544648e692d9930384e9
akshatqt <akshatgoel56@gmail.com>
2025-07-13 19:33:07 +0530

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.weatherforcastapp
+
+import android.os.Bundle
+import android.content.Intent
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var cityInput: EditText
+    private lateinit var nextButton: ImageButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)   // XML file you pasted
+
+        // made variable
+        cityInput  = findViewById(R.id.cityInput)
+        nextButton = findViewById(R.id.nextButton)
+
+        // enter pressed on keyboard
+        cityInput.setOnEditorActionListener { _, actionId, event ->
+            val isDone = actionId == EditorInfo.IME_ACTION_DONE
+            val isEnter = event?.keyCode == KeyEvent.KEYCODE_ENTER &&
+                    event.action == KeyEvent.ACTION_DOWN
+            if (isDone || isEnter) {
+                goToNextPage()
+                true
+            } else {
+                false
+            }
+        }
+
+        // arrow button
+        nextButton.setOnClickListener { goToNextPage() }
+    }
+
+    // checks if the input is empty or not
+    private fun goToNextPage() {
+        val cityName = cityInput.text.toString().trim()
+
+        if (cityName.isEmpty()) {
+            Toast.makeText(this, "Please enter a city first", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        // intent to go to next page
+
+        //val intent = Intent(this, WeatherActivity::class.java).apply {
+          //  putExtra("EXTRA_CITY", cityName)
+        //}
+        //startActivity(intent)
+    }
+}
