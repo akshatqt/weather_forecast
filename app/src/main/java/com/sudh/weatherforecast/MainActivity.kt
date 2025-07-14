25738774079074041ead1215cb115c63efd8ec09
hszinz <sudhanshulke@gmail.com>
2025-07-14 21:10:24 +0530

    added main screen + a few changes in SettingsActivity

--- app/src/main/java/com/sudh/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/sudh/weatherforecast/MainActivity.kt
@@ -1,20 +1,58 @@
 package com.sudh.weatherforecast
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.content.Intent
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var cityInput: EditText
+    private lateinit var nextButton: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.activity_main)   // XML file you pasted
+
+        // made variable
+        cityInput  = findViewById(R.id.cityInput)
+        nextButton = findViewById(R.id.nextButton)
+
+        // enter pressed on keyboard
+        cityInput.setOnEditorActionListener { _, actionId, event ->
+            val isDone = actionId == EditorInfo.IME_ACTION_DONE
+            val isEnter = event?.keyCode == KeyEvent.KEYCODE_ENTER &&
+                    event.action == KeyEvent.ACTION_DOWN
+            if (isDone || isEnter) {
+                goToNextPage()
+                true
+            } else {
+                false
+            }
         }
+
+        // arrow button
+        nextButton.setOnClickListener { goToNextPage() }
     }
+
+    // checks if the input is empty or not
+    private fun goToNextPage() {
+        val cityName = cityInput.text.toString().trim()
+
+        if (cityName.isEmpty()) {
+            Toast.makeText(this, "Please enter a city first", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        // If city is entered, proceed to next screen
+        val intent = Intent(this, HomeActivity::class.java)
+        intent.putExtra("city_name", cityName)
+        startActivity(intent)
+        finish()
+    }
+
 }
